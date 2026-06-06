cf7c221a9e931ebe90a8dc1d644c4ac7dfb13348
wrds <2598142880@qq.com>
2026-06-06 23:53:48 +0800

    chore(i18n): regenerate strings.g.dart after rebase

--- hibiki/lib/i18n/strings.g.dart
+++ hibiki/lib/i18n/strings.g.dart
@@ -3,7 +3,7 @@
 /// Locales: 17
 /// Strings: 23290 (1370 per locale)
 ///
-/// Built on 2026-06-06 at 15:38 UTC
+/// Built on 2026-06-06 at 15:53 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
